fe5274c6a06e23eca093ddeaadce934f0f192fcd
Allen <stupidme.me.lzy@gmail.com>
2017-09-16 14:11:01 +0800

    Create controllers and templates

--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,7 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.IndexController{})
+	beego.Router("/admin", &controllers.AdminController{})
+	beego.Router("/login", &controllers.LoginController{})
 }
