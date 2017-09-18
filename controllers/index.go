18e99e1aaee88afd41882e179361c591f79209c9
Allen <562117676@qq.com>
2017-09-18 22:51:27 +0800

    Inflate index page

--- controllers/index.go
+++ controllers/index.go
@@ -1,11 +1,27 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"time"
+)
 
 type IndexController struct {
 	beego.Controller
 }
 
-func (c *IndexController) Get(){
-	c.TplName = "index.tpl"
+type Blog struct {
+	Link  string
+	Title string
+	Time  time.Time
+}
+
+func (c *IndexController) Get() {
+	c.TplName = "index.html"
+
+	blogs := []Blog{
+		{"/blog/a", "Hello a", time.Now()},
+		{"/blog/b", "Hello b", time.Now()},
+	}
+
+	c.Data["Blogs"] = blogs
 }
